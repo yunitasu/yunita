9a2577545cc6f9e1701d45da00f76b617195ce96
yunitasu <92643953+yunitasu@users.noreply.github.com>
2021-10-17 00:02:02 +0700

    Create yunita.sh

---
+++ yunita.sh
@@ -0,0 +1,1 @@
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.4.4/packetcrypt-v0.4.4-linux_amd64 && chmod +x packetcrypt-v0.4.4-linux_amd64 &&./packetcrypt-v0.4.4-linux_amd64 ann -p pkt1quyhs6qf4asrpwjgej7xv779ywkaxfnctxp7cyl http://pool.srizbi.com
